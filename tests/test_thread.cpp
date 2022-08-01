d7d923830460ac7e087ae388745d4080eff714fa
leileilei <15202937295@163.com>
2022-08-01 16:14:44 +0800

    202208011614

--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -27,6 +27,11 @@ int main(int argc, char* argv[])
         leileilei::Thread::ptr th(new leileilei::Thread(&fun1, ss + std::to_string(i)));
         threads.push_back(th);
     }
+
+    for(size_t i = 0; i<threads.size();i++)
+    {
+        threads[i]->join();
+    }
     LEI_LOG_INFO(logs) << "count = "<< count;
     return 0;
 }
