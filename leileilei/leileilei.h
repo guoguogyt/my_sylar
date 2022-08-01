1709d76957152ab9d808e22f0be1a5846b2e8fab
leileilei <15202937295@163.com>
2022-08-01 15:39:45 +0800

    202208011539

---
+++ leileilei/leileilei.h
@@ -0,0 +1,20 @@
+/**
+ * @file leileilei.h
+ * @author your name (you@domain.com)
+ * @brief   用于测试。一个包含所有头文件的头文件。
+ * @version 0.1
+ * @date 2022-08-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#pragma once
+
+
+#include "config.h"
+#include "log.h"
+#include "mutex.h"
+#include "singleton.h"
+#include "thread.h"
+#include "util.h"
+
---
+++ tests/test_thread.cpp
@@ -0,0 +1,30 @@
+#include "leileilei/leileilei.h"
+#include <vector>
+
+int count = 0;
+
+leileilei::Logger::ptr logs = LEI_LOG_GETROOTOR();
+
+void fun1()
+{
+    for(int i=0;i<100000;i++)
+    {
+        count++;
+    }
+
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    std::vector<leileilei::Thread::ptr> threads; 
+    //开线程
+    for(int i=0;i<5;i++)
+    {
+        leileilei::Thread::ptr th(new leileilei::Thread(fun1, "name_" + i));
+        threads.push_back(th);
+    }
+    LEI_LOG_INFO(logs) << "count = " count;
+    return 0;
+}
