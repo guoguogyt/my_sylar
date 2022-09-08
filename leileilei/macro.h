dc5ca389e1befb53fd5baee51005fae1de01dc65
leileilei <15202937295@163.com>
2022-09-08 13:37:15 +0800

    202209081337

--- leileilei/macro.h
+++ leileilei/macro.h
@@ -16,6 +16,23 @@
 #include "log.h"
 #include "util.h"
 
+/**
+ *   long __builtin_expect(long exp, long c)函数是GCC的一个内建函数(build-in function)
+    exp：
+　　　　exp 为一个整型表达式, 例如: (ptr != NULL)
+    c：
+　　　　 c 必须是一个编译期常量, 不能使用变量
+    返回值：
+        返回值等于 第一个参数 exp
+
+现在 处理器 都是 流水线 的，系统可以 提前取多条指令 进行并行处理，但遇到跳转时，则需要重新取指令，跳转指令打乱了CPU流水线。
+因此，跳转次数少的程序拥有更高的执行效率。
+在C语言编程时，会不可避免地使用if-else分支语句，if else 句型编译后, 一个分支的汇编代码紧随前面的代码，而另一个分支的汇编代码需要使用JMP指令才能访问到。
+很明显通过JMP访问需要更多的时间, 在复杂的程序中,有很多的if else句型,又或者是一个有if else句型的库函数,每秒钟被调用几万次，通常程序员在分支预测方面做得很糟糕,
+ 编译器又不能精准的预测每一个分支,这时JMP产生的时间浪费就会很大。
+因此，引入__builtin_expect函数来增加条件分支预测的准确性，cpu 会提前装载后面的指令，遇到条件转移指令时会提前预测并装载某个分支的指令。
+编译器会产生相应的代码来优化 cpu 执行效率。
+ */
 #if defined __GNUC__ || defined __llvm__
 #   define LEILEILEI_LIKELY(x)      __builtin_expect(!!(x), 1)
 #   define LEILEILEI_UNLIKELY(x)    __builtin_expect(!!(x), 0)
--- tests/test_util.cpp
+++ tests/test_util.cpp
@@ -4,13 +4,18 @@
  * @Author: leileilei
  * @Date: 2022-09-08 11:11:38
  * @LastEditors: sueRimn
- * @LastEditTime: 2022-09-08 11:37:29
+ * @LastEditTime: 2022-09-08 13:36:36
  */
 // #include <assert.h>
 #include "leileilei.h"
 
+void fun()
+{
+    LEILEILEI_ASSERT(0);
+}
+
 int main(int argc, char* argv[])
 {
-    assert(1);
+    fun(1);
     return 0;
 }
