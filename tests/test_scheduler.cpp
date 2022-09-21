7c1015a15f15c293030875c6e354183265554c07
leileilei <15202937295@163.com>
2022-09-21 10:28:37 +0800

    202209211028

--- tests/test_scheduler.cpp
+++ tests/test_scheduler.cpp
@@ -4,7 +4,7 @@
  * @Author: leileilei
  * @Date: 2022-09-20 15:35:40
  * @LastEditors: sueRimn
- * @LastEditTime: 2022-09-21 10:08:24
+ * @LastEditTime: 2022-09-21 10:18:15
  */
 #include "leileilei.h"
 
@@ -20,10 +20,14 @@ int main(int argc, char* argv[])
     YAML::Node root = YAML::LoadFile("/root/share/my_sylar/bin/config/log.yml");
     leileilei::ConfigManager::LoadConfigFromYaml(root);
     LEI_LOG_DEBUG(g_logger) << "main start";
+    sleep(2);
     leileilei::Scheduler sc(3, true, "test_scheduler");
+    sleep(2);
     sc.start();
+    sleep(2);
     LEI_LOG_DEBUG(g_logger) << "main other things";
     sc.schedule(&test_fiber);
+    sleep(2);
     sc.stop();
     LEI_LOG_DEBUG(g_logger) << "main over";
 }
