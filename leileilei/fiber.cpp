d9cb5879521ec2383782e1653536c9b51dd155b2
leileilei <15202937295@163.com>
2022-09-21 10:06:25 +0800

    202209211006

--- leileilei/fiber.cpp
+++ leileilei/fiber.cpp
@@ -4,7 +4,7 @@
  * @Author: leileilei
  * @Date: 2022-08-22 15:33:45
  * @LastEditors: sueRimn
- * @LastEditTime: 2022-09-21 10:04:48
+ * @LastEditTime: 2022-09-21 10:05:52
  */
 #include "fiber.h"
 #include "log.h"
@@ -320,10 +320,8 @@ uint64_t Fiber::GetFiberId()
 {
     if(t_fiber)
     {
-        printf("111\n");
-        t_fiber->getId();
+        return t_fiber->getId();
     } 
-    
     return 0;
 }
 
