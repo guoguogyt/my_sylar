a899b6c3f02d742fb18426bfd04bf84f02726bca
leileilei <15202937295@163.com>
2022-11-07 13:49:00 +0800

    202211071348

--- tests/test_hook.cpp
+++ tests/test_hook.cpp
@@ -4,9 +4,14 @@
  * @Author: leileilei
  * @Date: 2022-10-27 09:32:16
  * @LastEditors: sueRimn
- * @LastEditTime: 2022-11-02 10:44:16
+ * @LastEditTime: 2022-11-07 13:48:25
  */
 #include "leileilei.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 leileilei::Logger::ptr g_logger = LEI_LOG_GETROOTOR();
 
@@ -31,9 +36,47 @@ void test_sleep()
     LEI_LOG_DEBUG(g_logger) << "test sleep";
 }
 
+void test_sock()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(80);
+    inet_pton(AF_INET, "110.242.68.66", &addr.sin_addr.s_addr);
+
+    LEI_LOG_DEBUG(g_logger) << "begin connct";
+
+    int rt = connect(sock, (const sockaddr*)&addr, sizeof(addr));
+    LEI_LOG_DEBUG(g_logger) << "connect rt = " << rt << "   errno=" << errno;
+
+    if(rt)
+        return;
+    
+    const char data[] = "GET / HTTP/1.0\r\n\r\n";
+    rt = send(sock, data, sizeof(data), 0);
+    LEI_LOG_DEBUG(g_logger) << "send rt=" << rt << "    errno" << errno;
+    if(rt<=0)
+        return;
+
+    std::string buff;
+    buff.resize(4096);
+
+    rt =recv(sock, &buff[0], buff.size(), 0);
+    LEI_LOG_DEBUG(g_logger) << "recv rt=" << rt << "    errno" << errno;
+    if(rt<=0)
+        return;
+    
+    buff.resize(rt);
+    LEI_LOG_DEBUG(g_logger) << buff;
+}
+
 int main()
 {
-    test_sleep();
+    // test_sleep();
+    test_sock();
     return 0;
 }
 
