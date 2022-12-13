48f62f836ecca12c59fc458944f6d29962bc9378
guoguogyt <15202937295@163.com>
2022-12-13 11:37:52 +0800

    202212131137

--- leileilei/endian.h
+++ leileilei/endian.h
@@ -1,91 +1,94 @@
-/**
- * @file endian.h
- * @brief 字节序操作函数(大端/小端)
- * @author sylar.yin
- * @email 564628276@qq.com
- * @date 2019-06-01
- * @copyright Copyright (c) 2019年 sylar.yin All rights reserved (www.sylar.top)
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: leileilei
+ * @Date: 2022-12-05 11:00:58
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2022-12-13 11:35:51
  */
-#ifndef __SYLAR_ENDIAN_H__
-#define __SYLAR_ENDIAN_H__
+#pragma once
 
-#define SYLAR_LITTLE_ENDIAN 1
-#define SYLAR_BIG_ENDIAN 2
+#define LEI_LITTLE_ENDIAN 1
+#define LEI_BIG_ENDIAN 2
 
 #include <byteswap.h>
 #include <stdint.h>
 
-namespace leileilei {
+namespace leileilei
+{
 
 /**
  * @brief 8字节类型的字节序转化
+ * @tparam T 
+ * @param value 
+ * @return std::enable_if<sizeof(T) == sizeof(uint64_t), T>::type 
  */
 template<class T>
 typename std::enable_if<sizeof(T) == sizeof(uint64_t), T>::type
-byteswap(T value) {
+byteswap(T value)
+{
     return (T)bswap_64((uint64_t)value);
 }
 
 /**
  * @brief 4字节类型的字节序转化
+ * @tparam T 
+ * @param value 
+ * @return std::enable_if<sizeof(T) == sizeof(uint32_t), T>::type 
  */
 template<class T>
 typename std::enable_if<sizeof(T) == sizeof(uint32_t), T>::type
-byteswap(T value) {
+byteswap(T value)
+{
     return (T)bswap_32((uint32_t)value);
 }
 
 /**
- * @brief 2字节类型的字节序转化
+ * @brief 2字节类型的字节序转化 
+ * @tparam T 
+ * @param value 
+ * @return std::enable_if<sizeof(T) == sizeof(uint16_t), T>::type 
  */
 template<class T>
 typename std::enable_if<sizeof(T) == sizeof(uint16_t), T>::type
-byteswap(T value) {
+byteswap(T value)
+{
     return (T)bswap_16((uint16_t)value);
 }
 
 #if BYTE_ORDER == BIG_ENDIAN
-#define SYLAR_BYTE_ORDER SYLAR_BIG_ENDIAN
+#define LEI_BYTE_ORDER LEI_BIG_ENDIAN
 #else
-#define SYLAR_BYTE_ORDER SYLAR_LITTLE_ENDIAN
+#define LEI_BYTE_ORDER LEI_LITTLE_ENDIAN
 #endif
 
-#if SYLAR_BYTE_ORDER == SYLAR_BIG_ENDIAN
-
-/**
- * @brief 只在小端机器上执行byteswap, 在大端机器上什么都不做
- */
-template<class T>
-T byteswapOnLittleEndian(T t) {
+// 当前机器是大端
+#if LEI_BYTE_ORDER == LEI_BIG_ENDIAN
+ template<class T>
+T byteswapOnLittleEndian(T t)
+{
     return t;
 }
 
-/**
- * @brief 只在大端机器上执行byteswap, 在小端机器上什么都不做
- */
 template<class T>
-T byteswapOnBigEndian(T t) {
+T byteswapOnBigEndian(T t)
+{
     return byteswap(t);
-}
-#else
+} 
 
-/**
- * @brief 只在小端机器上执行byteswap, 在大端机器上什么都不做
- */
+// 当前机器是小端
+#else 
 template<class T>
-T byteswapOnLittleEndian(T t) {
+T byteswapOnLittleEndian(T t)
+{
     return byteswap(t);
 }
 
-/**
- * @brief 只在大端机器上执行byteswap, 在小端机器上什么都不做
- */
 template<class T>
-T byteswapOnBigEndian(T t) {
+T byteswapOnBigEndian(T t)
+{
     return t;
 }
 #endif
 
 }
-
-#endif
