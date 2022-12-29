386f03cabe0f4f52dccdb360923a64419c145279
guoguogyt <15202937295@163.com>
2022-12-29 16:14:40 +0800

    202212291614

--- tests/test_bytearray.cpp
+++ tests/test_bytearray.cpp
@@ -4,7 +4,7 @@
  * @Author: leileilei
  * @Date: 2022-12-29 15:14:38
  * @LastEditors: sueRimn
- * @LastEditTime: 2022-12-29 15:33:04
+ * @LastEditTime: 2022-12-29 16:10:25
  */
 
 
@@ -54,6 +54,50 @@ void test()
     XX(uint64_t, 100, writeUint64, readUint64, 1);
 #undef XX
 
+
+#define XX(type, len, write_fun, read_fun, base_len) {\
+    std::vector<type> vec; \
+    for(int i = 0; i < len; ++i) { \
+        vec.push_back(rand()); \
+    } \
+    leileilei::ByteArray::ptr ba(new leileilei::ByteArray(base_len)); \
+    for(auto& i : vec) { \
+        ba->write_fun(i); \
+    } \
+    ba->setPosition(0); \
+    for(size_t i = 0; i < vec.size(); ++i) { \
+        type v = ba->read_fun(); \
+        LEILEILEI_ASSERT(v == vec[i]); \
+    } \
+    LEILEILEI_ASSERT(ba->getReadSize() == 0); \
+    LEI_LOG_DEBUG(g_logger) << #write_fun "/" #read_fun \
+                    " (" #type " ) len=" << len \
+                    << " base_len=" << base_len \
+                    << " size=" << ba->getSize(); \
+    ba->setPosition(0); \
+    LEILEILEI_ASSERT(ba->writeToFile("/tmp/" #type "_" #len "-" #read_fun ".dat")); \
+    sylar::ByteArray::ptr ba2(new sylar::ByteArray(base_len * 2)); \
+    LEILEILEI_ASSERT(ba2->readFromFile("/tmp/" #type "_" #len "-" #read_fun ".dat")); \
+    ba2->setPosition(0); \
+    LEILEILEI_ASSERT(ba->toString() == ba2->toString()); \
+    LEILEILEI_ASSERT(ba->getPosition() == 0); \
+    LEILEILEI_ASSERT(ba2->getPosition() == 0); \
+}
+    XX(int8_t,  100, writeFint8, readFint8, 1);
+    XX(uint8_t, 100, writeFuint8, readFuint8, 1);
+    XX(int16_t,  100, writeFint16,  readFint16, 1);
+    XX(uint16_t, 100, writeFuint16, readFuint16, 1);
+    XX(int32_t,  100, writeFint32,  readFint32, 1);
+    XX(uint32_t, 100, writeFuint32, readFuint32, 1);
+    XX(int64_t,  100, writeFint64,  readFint64, 1);
+    XX(uint64_t, 100, writeFuint64, readFuint64, 1);
+
+    XX(int32_t,  100, writeInt32,  readInt32, 1);
+    XX(uint32_t, 100, writeUint32, readUint32, 1);
+    XX(int64_t,  100, writeInt64,  readInt64, 1);
+    XX(uint64_t, 100, writeUint64, readUint64, 1);
+
+#undef XX
 }
 
 int main(int argc, char* argv[])
