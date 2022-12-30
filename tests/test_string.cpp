c14da94b4cc95a2022506f9c318d50e431ee57b9
guoguogyt <15202937295@163.com>
2022-12-30 14:47:23 +0800

    202212301447

---
+++ tests/test_string.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: leileilei
+ * @Date: 2022-12-30 13:49:51
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2022-12-30 14:04:44
+ */
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
